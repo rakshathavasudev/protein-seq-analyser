efff6a7884af56c1cf6f38e79cbd1b163946487f
madhumithanara <38850744+madhumithanara@users.noreply.github.com>
2019-10-10 01:32:51 +0530

    Replicated functionality to all five models (#2)

--- protseqanalyser/foldrecog/urls.py
+++ protseqanalyser/foldrecog/urls.py
@@ -1,5 +1,6 @@
 from . import views
 from django.conf.urls import url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from . import views
 
 app_name = 'foldrecog'
@@ -7,3 +8,5 @@ app_name = 'foldrecog'
 urlpatterns = [
     url(r'^$', views.predict, name="predict"),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
--- protseqanalyser/foldrecog/views.py
+++ protseqanalyser/foldrecog/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import models
+from . import forms
 
 # Create your views here.
 def predict(request):
-    return HttpResponse('Fold Recog')
+    if request.method == 'POST':
+        form = forms.ProteinSequenceForm(request.POST)
+        if form.is_valid():
+            print("here")
+            form.save()
+            return render(request, 'foldrecog/process.html')
+    else:
+        form = forms.ProteinSequenceForm
+    return render(request, 'foldrecog/predict.html', {'form':form})
