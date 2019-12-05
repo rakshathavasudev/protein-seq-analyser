14eab48d0f4ac2a7b62a0412656ee5a345318d40
madhumithanara <38850744+madhumithanara@users.noreply.github.com>
2019-12-05 20:00:18 +0530

    Implemented counter (#8)

--- protseqanalyser/protseqanalyser/views.py
+++ protseqanalyser/protseqanalyser/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+visitors = 0
+
 # Create your views here.
 def home(request):
-    return render(request,'home.html')
+        global visitors
+
+        request.session.set_expiry(0) #Session ends when the browser is closed
+        
+        if request.session.get('has_visited') == None:
+                visitors+=1
+                request.session['has_visited'] = True
+
+        context = {"visitors": visitors}
+        return render(request,'home.html',context)
