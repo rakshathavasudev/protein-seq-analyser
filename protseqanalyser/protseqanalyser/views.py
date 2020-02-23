8de367a356f81ec28585fcb96eccab290b4bdc8d
SaurabhAgarwala <saur.agarwala@gmail.com>
2020-02-23 17:20:58 +0530

    Solved visitor count bug

--- protseqanalyser/protseqanalyser/views.py
+++ protseqanalyser/protseqanalyser/views.py
@@ -1,19 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from visitor.models import Visitors
+from django.db.models import F
 
 # Create your views here.
 def home(request):
 
         request.session.set_expiry(0) #Session ends when the browser is closed
         try:
-                visitor = Visitors.objects.filter(id=1)
+                visitor = Visitors.objects.get(id=0)
         except:
-                visitor = Visitors()
+                visitor = Visitors(id=0)
                 visitor.save()
 
         if request.session.get('has_visited') == None:
-                visitors.visitor_count+=1
+                visitor.visitor_count = F('visitor_count') + 1
                 request.session['has_visited'] = True
+                visitor.save()
 
-        context = {"visitors": visitors.visitor_count}
+        context = {"visitors": visitor.visitor_count}
         return render(request,'home.html',context)
